3bc02f243b3839833e6f4d9aacac15abc7aacd01
artem.kel <artem.kel@datalogue.de>
2023-10-24 16:55:13 +0200

    add reports folder, gitignore, and flat.py

--- advanced_python_tutorial/App-2-Flatmates-Bill/main.py
+++ advanced_python_tutorial/App-2-Flatmates-Bill/main.py
@@ -1,31 +1,6 @@
+import webbrowser
 from fpdf import FPDF
-
-
-class Bill:
-    """
-    Object that contains data about a bill,
-    such as total amount and period of the bill.
-    """
-
-    def __init__(self, amount, period):
-        self.amount = amount
-        self.period = period
-
-
-class Flatmate:
-    """
-    Creates a flatmate person who lives in the
-    flat and pays a share of the bill.
-    """
-
-    def __init__(self, name, days_in_house):
-        self.name = name
-        self.days_in_house = days_in_house
-
-    def pays(self, bill, flatmate2):
-        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
-        to_pay = round(bill.amount * weight, 1)
-        return to_pay
+from flat import Bill, Flatmate
 
 
 class PdfReport:
@@ -47,29 +22,48 @@ class PdfReport:
 
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmate bill', border=1, align='C', ln=1)
+        pdf.cell(w=0, h=140, txt='Flatmate bill', border=0, align='C', ln=1)
 
         # Insert Period label and value
-        pdf.cell(w=100, h=40, txt='Period:', border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         # Insert name and due amount of the 1st flatmate
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt='$' + str(flatmate1.pays(bill, flatmate2)), border=0, ln=1)
 
         # Insert name and due amount of the 2nd flatmate
-        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate2.pays(bill, flatmate1)), border=1, ln=1)
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt='$' + str(flatmate2.pays(bill, flatmate1)), border=0, ln=1)
+
+        # Insert total bill amount
+        pdf.set_font(family='Times', size=12, style='B')
+        pdf.cell(w=100, h=40, txt='Total', border=0)
+        pdf.cell(w=150, h=40, txt='$' + str(bill.amount), border=0, ln=1)
 
         pdf.output(self.filename)
+        webbrowser.open(self.filename)
 
 
-the_bill = Bill(amount=120, period="April 2021")
-john = Flatmate(name='John', days_in_house=20)
-marry = Flatmate(name='Marry', days_in_house=25)
+period = input("Enter the period (Month-YYYY): ")
+amount = float(input("Enter the bill amount in USD: "))
 
-print(john.pays(bill=the_bill, flatmate2=marry))
-print(marry.pays(bill=the_bill, flatmate2=john))
+flatmate1_name = input("What's the 1st flatmate's name? ")
+flatmate1_days = int(input(f"How many days did {flatmate1_name} spend home in {period}? "))
 
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
+flatmate2_name = input("What's the 2nd flatmate's name? ")
+flatmate2_days = int(input(f"How many days did {flatmate2_name} spend home in {period}? "))
+
+
+the_bill = Bill(amount=amount, period=period)
+flatmate1 = Flatmate(name=flatmate1_name, days_in_house=flatmate1_days)
+flatmate2 = Flatmate(name=flatmate2_name, days_in_house=flatmate2_days)
+
+print(f"{flatmate1.name} pays: {flatmate1.pays(bill=the_bill, flatmate2=flatmate2)} USD")
+print(f"{flatmate2.name} pays: {flatmate2.pays(bill=the_bill, flatmate2=flatmate1)} USD")
+
+pdf_report = PdfReport(filename=f"reports\Report_{period}.pdf")
+pdf_report.generate(flatmate1=flatmate1, flatmate2=flatmate2, bill=the_bill)
