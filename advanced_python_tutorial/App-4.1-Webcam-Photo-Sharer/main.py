ec41233ba6d19b814b13018360a59576a92a7cc5
artem.kel <artem.kel@datalogue.de>
2023-10-27 11:20:55 +0200

    <ImageScreen> added

--- advanced_python_tutorial/App-4.1-Webcam-Photo-Sharer/main.py
+++ advanced_python_tutorial/App-4.1-Webcam-Photo-Sharer/main.py
@@ -10,18 +10,31 @@ Builder.load_file(('frontend.kv'))
 
 class CameraScreen(Screen):
     def start(self):
+        """
+        Starts camera and changes Button text
+        """
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        """
+        Stops camera and changes Button text
+        """
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
 
     def capture(self):
+        """
+        Creates a filename withe the current time and captures
+        and saves a photo image under that filename
+        """
         current_time = datetime.now()
-        self.ids.camera.export_to_png(f"images/img_{current_time.strftime('%Y%m%d_%H%M%S')}.png")
+        filepath = f"images/img_{current_time.strftime('%Y%m%d_%H%M%S')}.png"
+        self.ids.camera.export_to_png(filepath)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = filepath
 
 
 class ImageScreen(Screen):
