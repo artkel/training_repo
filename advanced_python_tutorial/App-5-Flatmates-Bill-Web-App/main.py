106e2bdd0745412c894ddc706c47f9d024604570
artem.kel <artem.kel@datalogue.de>
2023-10-27 15:11:08 +0200

    home page updated

--- advanced_python_tutorial/App-5-Flatmates-Bill-Web-App/main.py
+++ advanced_python_tutorial/App-5-Flatmates-Bill-Web-App/main.py
@@ -1,19 +1,19 @@
 from flask.views import MethodView
 from wtforms import Form
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 class HomePage(MethodView):
 
     def get(self):
-        return 'Hello'
+        return render_template('index.html')
 
 
 class BillFormPage(MethodView):
 
     def get(self):
-        return 'I am the bill form page'
+        return '<h2>I am the bill form page</h2>'
 
 
 class ResultsPage(MethodView):
@@ -27,4 +27,4 @@ class BillForm(Form):
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
 
-app.run()
+app.run(debug=True)
