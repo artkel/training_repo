44281ab1873477723964e54c124e0492107bb995
artem.kel <artem.kel@datalogue.de>
2023-10-28 20:23:36 +0200

    app first version finished

--- advanced_python_tutorial/App-5-Flatmates-Bill-Web-App/main.py
+++ advanced_python_tutorial/App-5-Flatmates-Bill-Web-App/main.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flatmates_bill import flat
 
 app = Flask(__name__)
 
@@ -19,20 +20,30 @@ class BillFormPage(MethodView):
 
 class ResultsPage(MethodView):
 
-    def get(self):
-        return "Here we will see the results"
+    def post(self):
+        billform = BillForm(request.form)
+
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return render_template('results.html',
+                               name1=flatmate1.name,
+                               amount1=f"$ {round(flatmate1.pays(the_bill, flatmate2), 2)}",
+                               name2=flatmate2.name,
+                               amount2=f"$ {round(flatmate2.pays(the_bill, flatmate1), 2)}")
 
 
 class BillForm(Form):
 
-    amount = StringField("Bill Amount: ")
-    period = StringField("Period: ")
+    amount = StringField("Bill Amount: ", default=100)
+    period = StringField("Period: ", default='December 2020')
 
-    name1 = StringField("Name: ")
-    days_in_house1 = StringField("Days in the house: ")
+    name1 = StringField("Name: ", default='John')
+    days_in_house1 = StringField("Days in the house: ", default=21)
 
-    name2 = StringField("Name: ")
-    days_in_house2 = StringField("Days in the house: ")
+    name2 = StringField("Name: ", default='Anna')
+    days_in_house2 = StringField("Days in the house: ", default=14)
 
     button = SubmitField("Calculate")
 
